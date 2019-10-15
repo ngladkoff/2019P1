712428e9d06fb594e9a792def310223cc57b1014
Nicolas Gladkoff <ngladkoff@users.noreply.github.com>
2019-10-15 14:42:56 -0300

    Agenda

--- agenda.py
+++ agenda.py
@@ -10,12 +10,22 @@ class Contacto:
         return "{0} - {1} {2}".format(self.id, self.nombre, self.apellido)
 
 listaContactos = []
-listaContactos.append(Contacto(1, "Juan", "Perez", "1234"))
-listaContactos.append(Contacto(2, "Alberto", "Perez", "1234"))
-listaContactos.append(Contacto(3, "Carlos", "Gomez", "1234"))
+proximoID= 0
 
+try:
+    with open('agenda.csv', 'r') as agenda:
+    
+        for linea in agenda:
+            cnt= linea.split('|')
+            listaContactos.append(Contacto(int(cnt[0]), cnt[1], cnt[2], cnt[3]))
+            if (int(cnt[0]) > proximoID):
+                proximoID = int(cnt[0])
 
-proximoID= 4
+except:
+    with open('agenda.csv', 'w') as agenda:
+        agenda.write('')
+        
+proximoID = proximoID + 1
 
 op= 0
 txtMenu= """
@@ -42,11 +52,40 @@ def AltaContacto(lstCont, proximoID):
     proximoID = proximoID + 1
     return proximoID
 
-def BajaContacto():
-    pass
+def BajaContacto(lstContactos):
+    BuscarContacto(lstContactos)
+    id= int(input("Ingrese ID a borrar: "))
+    
+    contactoABorrar= Contacto(0,"","","")
+    encontrado= False
+    for i in range(0,len(lstContactos)):
+        if lstContactos[i].id== id:
+            contactoABorrar= lstContactos[i]
+            encontrado= True
+            break
+    if encontrado == True:
+        lstContactos.remove(contactoABorrar)
 
-def EditarContacto():
-    pass
+def EditarContacto(lstContactos):
+    BuscarContacto(lstContactos)
+    # idx = BuscarID()
+    id= int(input("Ingrese ID a editar: "))
+    
+    contactoAEditar= Contacto(0,"","","")
+    encontrado= False
+    for i in range(0,len(lstContactos)):
+        if lstContactos[i].id== id:
+            # lstContactos[i].nombre = input("Ingrese nombre: ")
+            contactoAEditar= lstContactos[i]
+            encontrado= True
+            break
+    if encontrado == True:
+        print("{3} - {0} {1}: {2}".format(contactoAEditar.nombre, contactoAEditar.apellido, contactoAEditar.telefono, contactoAEditar.id))
+        contactoAEditar.nombre = input("Ingrese nuevo nombre: ")
+        contactoAEditar.apellido= input("Ingrese nuevo apellido: ")
+        contactoAEditar.telefono= input("Ingrese nuevo telefono: ")
+        
+    
 
 def BuscarContacto(lstContactos):
     
@@ -56,7 +95,7 @@ def BuscarContacto(lstContactos):
     for i in range(0, len(lstContactos)):
         c= lstContactos[i]
         if c.apellido == apellido:
-            print("{0} {1}: {2}".format(c.nombre, c.apellido, c.telefono))
+            print("{3} - {0} {1}: {2}".format(c.nombre, c.apellido, c.telefono, c.id))
             encontrado= True
     
     if encontrado == False:
@@ -69,11 +108,18 @@ while (op != 5):
     op= int(input("Ingrese opción: "))
     if (op == 1):
         proximoID= AltaContacto(listaContactos, proximoID)
+        input("Presione Enter para continuar...")
     if (op == 2):
-        BajaContacto()
+        BajaContacto(listaContactos)
+        input("Presione Enter para continuar...")
     if (op == 3):
         EditarContacto()
+        input("Presione Enter para continuar...")
     if (op == 4):
         BuscarContacto(listaContactos)
+        input("Presione Enter para continuar...")
 
 # Salir
+with open('agenda.csv', 'w') as agenda:
+    for i in range(0, len(listaContactos)):
+        agenda.write("{0}|{1}|{2}|{3}\n".format(listaContactos[i].id, listaContactos[i].nombre, listaContactos[i].apellido, listaContactos[i].telefono))
